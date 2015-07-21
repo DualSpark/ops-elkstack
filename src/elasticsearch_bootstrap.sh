bb7a9e89b6b9d3424850a010ff0c65fd96ed0b30
Matthew Mayer <matthewkmayer@gmail.com>
2015-07-21 15:13:36 -0700

    Adds aws plugin to elasticsearch nodes for autodiscovery.

--- src/elasticsearch_bootstrap.sh
+++ src/elasticsearch_bootstrap.sh
@@ -12,5 +12,11 @@ enabled=1
 EOF
 
 yum -y install elasticsearch
+service elasticsearch restart
+/usr/share/elasticsearch/bin/plugin install elasticsearch/elasticsearch-cloud-aws/2.7.0
+
+echo "discovery: " >> /etc/elasticsearch/elasticsearch.yml
+echo "  type: ec2" >> /etc/elasticsearch/elasticsearch.yml
+echo "discovery.ec2.tag.stage: dev" >> /etc/elasticsearch/elasticsearch.yml
 
 service elasticsearch restart

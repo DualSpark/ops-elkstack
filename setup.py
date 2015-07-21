57c517c477b94b4ba43b128df3ccb9f69ee5984d
Matthew Mayer <matthewkmayer@gmail.com>
2015-07-21 12:53:52 -0700

    Locks down envbase dep to 0.5.0.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name="elkstack",
     version="0.1",
     install_requires=[
-        'cfn-environment-base'
+        'cfn-environment-base==0.5.0'
     ],
     dependency_links=[
-        'https://github.com/DualSpark/cloudformation-environmentbase/zipball/feature-0.5.0#egg=cfn-environment-base'
+        'https://github.com/DualSpark/cloudformation-environmentbase/zipball/feature-0.5.0#egg=cfn-environment-base-0.5.0'
     ],
     package_dir={"": "src"},
     entry_points={

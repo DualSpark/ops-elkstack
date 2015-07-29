8c9b1c07a09ed9cc8686eb81ee1befadb56bb337
Matthew Mayer <matthewkmayer@gmail.com>
2015-07-29 13:37:39 -0700

    Switches to latest working envbase version.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name="elkstack",
     version="0.1",
     install_requires=[
-        'cfn-environment-base==0.5.0'
+        'cfn-environment-base==0.5.1'
     ],
     dependency_links=[
-        'https://github.com/DualSpark/cloudformation-environmentbase/archive/0.5.0.zip#egg=cfn-environment-base-0.5.0'
+        'https://github.com/DualSpark/cloudformation-environmentbase/archive/0.5.1.zip#egg=cfn-environment-base-0.5.1'
     ],
     package_dir={"": "src"},
     include_package_data=True,
